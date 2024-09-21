a891b0c3241faaeb60500fa781d46cc282965345
Heorhii Lytvynenko <104064704+Grifion@users.noreply.github.com>
2024-09-21 11:27:49 +0200

    make build

---
+++ index.c71d5427.js
@@ -0,0 +1,2 @@
+var e=document.getElementById("largeImg");document.getElementById("thumbs").addEventListener("click",function(t){t.preventDefault();var n=t.target.closest(".list-item").children[0];e.src=n.href});
+//# sourceMappingURL=index.c71d5427.js.map
